b3e138dadd9cc6732596da5e4ec0f67b4f142ea9
nguyentson <88874005+nguyentson@users.noreply.github.com>
2021-08-13 13:01:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `
+    
+    
+`
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'Dcu m đấm nhau khonggg',
+    desc: 'Trả lời t ngayyyy!!!! ',
+    btnYes: ' Rén lắm k đấm đâuu',
+    btnNo: ' Ok đấm thì đấm chuẩn bị tinh thần đi',
+    question:'Trên thế giới hơn 7 tỉ người mà m lại gạ t là sai lầm roi',
+    btnReply: ' Chuẩn bị tinh thần lm bao cát chưa',
+    reply: ' Đấm thì đấm mà k đấm thì đấm😏',
+    mess: ' Ok nhe ',
+    messDesc: ' Tối nay 7h lên sàn đấm nhau nhé.',
+    btnAccept: 'Okiiiii lun ',
+    messLink: 'https://www.facebook.com/tson3110' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
